07202491839077302b78fb61249012ecb429df0b
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-07-24 16:49:03 +0530

    bankingapp : react setup

---
+++ banking-front-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>
+    This Banking App
+  </h1>
+);
+
+
